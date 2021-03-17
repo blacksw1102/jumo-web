7dfb7ec2eaa2615df7e65a8c4b527bdb38752119
suwan kim <blacksw1102@gmail.com>
2021-03-17 22:48:32 +0900

    Add: 404 처리

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueRouter from 'vue-router'
+
+import SignUpForm from './components/SignUpForm'
+import PageNotFound from './views/PageNotFound'
+
+Vue.use(VueAxios, axios);
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/signup', SignUpForm },
+    { path: '*', redirect: "/404" },
+    { path: '/404', component: PageNotFound }
+  ]
+})
 
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 })
